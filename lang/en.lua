c3228b78570698c9379209a1d82398fa8a6d88e1
chillnook <83742340+chillnook@users.noreply.github.com>
2026-01-04 05:00:34 -0500

    Add files via upload

---
+++ lang/en.lua
@@ -0,0 +1,14 @@
+Locales = Locales or {}
+
+Locales['en'] = {
+    usageTitle = '^1Actions',
+    usageFormat = 'Usage: /%s <message>',
+
+    suggestionMe = 'Perform a roleplay emote',
+    suggestionDo = 'Describe the environment or state',
+    suggestionHelpMe = 'Describe your action',
+    suggestionHelpDo = 'Describe scene/state',
+
+    spamPrefix = 'SPAM',
+    spamMessage = 'You are sending actions too quickly. Please slow down.',
+}
---
+++ lang/es.lua
@@ -0,0 +1,14 @@
+Locales = Locales or {}
+
+Locales['es'] = {
+    usageTitle = '^1Acciones',
+    usageFormat = 'Uso: /%s <mensaje>',
+
+    suggestionMe = 'Realiza una acción de rol',
+    suggestionDo = 'Describe el entorno o estado',
+    suggestionHelpMe = 'Describe tu acción',
+    suggestionHelpDo = 'Describe la escena/estado',
+
+    spamPrefix = 'SPAM',
+    spamMessage = 'Estás enviando acciones demasiado rápido. Por favor, baja el ritmo.',
+}
